c720b680cedd7d828f79719ddcaf8575867e0c65
Davide Scarioni <davide.scarioni@gmail.com>
2024-05-14 23:04:21 +0200

    Update service worker and manifest

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,44 @@
-// Installazione del service worker
+const CACHE_NAME = 'sfida-classi-cache-v1';
+const urlsToCache = [
+  '/',
+  '/sfida-classi/index.html',
+  '/sfida-classi/icons/icon.png',
+];
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('offline-cache').then(function(cache) {
-      return cache.addAll([
-        '/', // La tua home page
-        '/index.html', // Assicurati di includere tutte le pagine che vuoi rendere disponibili offline
-        // Aggiungi qui tutte le risorse che vuoi memorizzare nella cache per l'accesso offline
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-// Interceptor per gestire le richieste di risorse
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
 
-// Change this to your repository name
-var GHPATH = '/sfida-classi';
- 
-// Choose a different app prefix name
-var APP_PREFIX = 'gppwa_';
- 
-// The version of the cache. Every time you change any of the files
-// you need to change this version (version_01, version_02…). 
-// If you don't change the version, the service worker will give your
-// users the old files!
-var VERSION = 'version_00';
- 
-// The files to make available for offline use. make sure to add 
-// others to this list
-var URLS = [    
-  `${GHPATH}/`,
-  `${GHPATH}/index.html`
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['sfida-classi-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
